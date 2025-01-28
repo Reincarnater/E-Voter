2db0689f6829e3201e966e6b531512ad6a8dde7c
Jougi Albiso <albisojougi03@gmail.com>
2025-01-28 21:53:07 +0900

    Update main.py to allow dynamic port configuration from environment variable

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     
 
