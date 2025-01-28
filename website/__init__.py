ad005f380e6dbe8697bf152ca1387dec4f819635
Jougi Albiso <albisojougi03@gmail.com>
2025-01-28 22:12:43 +0900

    Update upload folder path in create_app for correct directory structure

--- website/__init__.py
+++ website/__init__.py
@@ -12,7 +12,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['UPLOAD_FOLDER'] = 'static/images/'
+    app.config['UPLOAD_FOLDER'] = '/static/images/'
     app.config['SECRET_KEY'] = 'Please give us a passing grade :)'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_BINDS'] = {'candidates' : 'sqlite:///Candidates.db', 
