de225196072c4f06c6de206e0a48b15cc76baf52
Jougi Albiso <albisojougi03@gmail.com>
2025-03-12 13:47:06 +0800

    Last last last last last commit of e-voter

--- website/__init__.py
+++ website/__init__.py
@@ -1,30 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 from os import path
 from flask_login import LoginManager
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 migrate = Migrate()
 
-
-
-
 def create_app():
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 'images')
-    app.config['SECRET_KEY'] = 'Please give us a passing grade :)'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_BINDS'] = {'candidates' : 'sqlite:///Candidates.db', 
-                                      'teachers' : 'sqlite:///teachers.db'}
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', f'sqlite:///{DB_NAME}')
+    app.config['SQLALCHEMY_BINDS'] = {
+        'candidates': os.environ.get('CANDIDATES_DB_URL', 'sqlite:///Candidates.db'),
+        'teachers': os.environ.get('TEACHERS_DB_URL', 'sqlite:///teachers.db')
+    }
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     path = app.config['UPLOAD_FOLDER']
     if not os.path.exists(path):
         os.makedirs(path)
-    
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -56,7 +54,7 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
-    
+
 
 
 
